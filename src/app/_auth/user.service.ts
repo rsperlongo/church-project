06c545280c10210d8bfc15d8493ec9183377c8c7
rsperlongo <ricardo_tadeu30@hotmail.com>
2018-05-06 15:10:35 -0300

    auth update

---
+++ src/app/_auth/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { AuthenticationService } from "./authentication.service";
+import { Observable } from 'rxjs/Observable';
+import { User } from "../models/user";
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+
+export class UserService {
+    constructor(
+        http: Http,
+        authenticationService: AuthenticationService,
+    ){}
+
+    getUsers(): Observable<User[]>{
+        let headers = new Headers({ 'Authorization': 'Bearer' + this.authenticationService.token});
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.get('/api/users', options)
+        .map((response: Response)=> response.json());
+        }
+    }
