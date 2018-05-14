9bc9ce95d885bf162a14899962ff191017e93a11
rsperlongo <ricardo_tadeu30@hotmail.com>
2018-05-14 15:51:54 -0300

    update membros CRUD

--- src/app/membros-detalhes/membros-detalhes.component.ts
+++ src/app/membros-detalhes/membros-detalhes.component.ts
@@ -3,6 +3,7 @@ import { Membros } from '../membros/membros';
 import { MembrosService } from '../membros.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-membros-detalhes',
@@ -12,7 +13,25 @@ import { Location } from '@angular/common';
 export class MembrosDetalhesComponent implements OnInit {
   @Input() membros: Membros;
 
-  constructor( private route: ActivatedRoute,  private membrosService: MembrosService, private location: Location) { }
+  updateForm: FormGroup; 
+
+  constructor( private route: ActivatedRoute,  
+               private membrosService: MembrosService, 
+               private location: Location,
+               private fb: FormBuilder) { 
+                 this.createrForm();
+               }
+  
+  createrForm(){
+    this.updateForm = this.fb.group({
+      membroNome: ['', Validators.required ],
+      membroTelefone: ['', Validators.required ],
+      membroEndereco: ['', Validators.required ],
+      membroEmail: ['', Validators.required ],
+      membroDataNascimento: ['', Validators.required],
+      membroDataBatismo: ['', Validators.required],
+    })
+  }
 
   ngOnInit() {
     this.getMembro()
